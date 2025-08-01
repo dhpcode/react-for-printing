ca2d5392d063297a8feb082cc02f1ae7e9f6f254
davrsirius <davrsirius@gmail.com>
2025-08-02 00:40:23 +0500

    Data printed as list

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [name, setName] = useState('Davron')
+  const [name, setName] = useState('Davron');
+
+  const [events, setEvents] = useState([
+    { title: "akhror's birthday party", id: 1 },
+    { title: "doniyor's live stream", id: 2 },
+    { title: "match: manchester united vs barcelona", id: 3 },
+    { title: 'AFC annual ceremony', id: 4 }
+  ])
+
   const handleClick = () => {
     setName('Aziz');
   }
@@ -11,6 +19,15 @@ function App() {
     <div className="App">
       <h1>My name is {name}</h1>
       <button onClick={handleClick}>Change name</button>
+      <hr />
+      {events.map((event) => {
+        return (
+          <div key={event.id}>
+            <h2>{event.title}</h2>
+          </div>
+        )
+      })}
+
     </div>
   );
 }
