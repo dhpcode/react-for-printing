d26653cae10f10e5c7b03bcf1133b42cea3729b0
davrsirius <davrsirius@gmail.com>
2025-08-03 13:25:28 +0500

    added component and global styles

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   const [showContent, setShowContent] = useState(true);
 
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
 
 
   const handleDelete = (id) => {
