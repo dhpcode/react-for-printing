29a50a5aebabf9aded7b96b8f09abfa9e8ebc616
davrsirius <davrsirius@gmail.com>
2025-08-04 05:51:33 +0500

    style: update NewEventForm to manage input values and add reset functionality

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -4,16 +4,24 @@ import './NewEventForm.css'
 function NewEventForm() {
   const [title, setTitle] = useState('');
   const [date, setDate] = useState('');
+
+  const resetInputs = () => {
+    setTitle('');
+    setDate('');
+  }
+
   return (
     <form className='new-event-form'>
       <label>
         <span>Event Title</span>
-        <input type="text" onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" onChange={(e) => setTitle(e.target.value)} value={title}/>
       </label>
       <label>
         <span>Event Date</span>
-        <input type="date" onChange={e => setDate(e.target.value)}/>
+        <input type="date" onChange={e => setDate(e.target.value)} value={date} />
       </label>
+      <button type="button" onClick={resetInputs}>Reset input</button>
+      <br /><br />
       <button>Submit</button>
       <hr />
       <p>Title: {title}</p>
