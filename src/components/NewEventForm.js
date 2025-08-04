d1a9a805c0ea57115f11a26a952c163f2163a58b
davrsirius <davrsirius@gmail.com>
2025-08-04 05:42:58 +0500

    onChange eventi orqali statega ma'lumot saqlandi

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,17 +1,23 @@
+import { useState } from 'react'
 import './NewEventForm.css'
 
 function NewEventForm() {
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState('');
   return (
     <form className='new-event-form'>
       <label>
         <span>Event Title</span>
-        <input type="text" />
+        <input type="text" onChange={(e) => setTitle(e.target.value)} />
       </label>
       <label>
         <span>Event Date</span>
-        <input type="date" />
+        <input type="date" onChange={e => setDate(e.target.value)}/>
       </label>
       <button>Submit</button>
+      <hr />
+      <p>Title: {title}</p>
+      <p>Date: {date}</p>
     </form>
   )
 }
